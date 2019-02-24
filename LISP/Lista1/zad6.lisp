8d80b9f414a0f3854be4e18bd1ba6a84c0fbbd75
Jacek Noga <jaceknoga11@gmail.com>
2019-02-24 15:32:04 +0100

    Added exercise 3 and 6

--- LISP/Lista1/zad6.lisp
+++ LISP/Lista1/zad6.lisp
@@ -1,15 +1,40 @@
-(defun primes (num)
-
-(setq count 0.0)
-(while (< count num )
-(setq div_double (/ num count))
-(setq modulus (rem num count))
-(if (= modulous 0)
-(member factors div_double)
+(defun prime(n)
+    (if (<= n 1)
+        nil
+        (progn
+            (let (result)
+            (setf result t)
+            (loop for i from 2 to (- n 1)
+                if (= 0 (mod n i))
+                    do (setf result nil)      
+            )
+            result
+            )
+        )
+    )  
 )
 
-(setq factors (append factors (list div_double)))
+;(print(prime 1))
+;(print(prime 2))
+;(print(prime 3))
+;(print(prime 4))
+;(print(prime 5))
 
+(defun rozklad(n)
+    (let (factors)
+        (loop for i from 1 to n
+            if (and (= 0 (mod n i)) (prime i))
+            do
+                (progn
+                    (push i factors)
+                    (setf n (/ n i))
+                    (setf i 1) ;moze byc kilka takich samych dzielnikow
+                )
+        )
+        (reverse factors)
+    )
 )
 
-)
+(print (rozklad 1))
+(print (rozklad 6))
+(print (rozklad 24))
